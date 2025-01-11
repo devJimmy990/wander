8d0846fa5eebdcb69907862fcc5d7f8e6e126c75
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-01-12 00:49:30 +0200

    feat: create favorite page and needed widgets

---
+++ lib/presentation/widgets/custom_text_field.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final bool obscureText;
+  final String? Function(String?)? validator;
+
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    this.obscureText = false,
+    this.validator,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+        border: const OutlineInputBorder(),
+      ),
+      obscureText: obscureText,
+      validator: validator,
+    );
+  }
+}
