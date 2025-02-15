d7265836497d9aa7e03f67e90067c8c5cb7d1ba2
Ahmed Roshdy Ragheb Mohamed <ahmedroshdy6555554@gmail.com>
2025-02-15 23:36:06 +0200

    commit adding finger print auth

--- lib/controller/cubit/favorite/favorite_cubit.dart
+++ lib/controller/cubit/favorite/favorite_cubit.dart
@@ -1,14 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wander/controller/cubit/favorite/favorite_state.dart';
 import 'package:wander/data/model/item.dart';
 
 class FavoriteCubit extends Cubit<FavoriteState> {
   final List<Item> _favorites = [];
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   FavoriteCubit() : super(FavoriteInitial());
 
-  void onAddToFavorites(Item item) {
+  Future<void> onAddToFavorites(Item item, String userId) async {
     if (!_favorites.contains(item)) {
       _favorites.add(item);
+      await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection('favorites')
+          .doc(item.title)
+          .set({
+        'title': item.title,
+        'governorate': item.governorate,
+      });
       emit(FavoriteLoaded(_favorites));
     }
   }
