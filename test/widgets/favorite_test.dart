5b8df54a657a656c8a6005a3b011c24cabd8b42a
Ahmed Roshdy <ahmedroshdy654@gmail.com>
2025-02-23 13:04:20 +0200

    commit update widget tests

--- test/widgets/favorite_test.dart
+++ test/widgets/favorite_test.dart
@@ -1,39 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wander/controller/cubit/user/user_cubit.dart';
 import 'package:wander/controller/cubit/favorite/favorite_cubit.dart';
-import 'package:wander/presentation/features/favorite/page.dart';
+import 'package:wander/controller/cubit/user/user_cubit.dart';
 import 'package:wander/data/model/user.dart';
+import 'package:wander/presentation/features/favorite/page.dart';
 
-class FakeUserCubit extends UserCubit {
+/// A fake UserCubit for when the user is not logged in.
+class FakeUserCubitLoggedOut extends UserCubit {
   @override
-  get user => User(id: "123", name: "Test User", email: "test@gmail.com");
+  get user => null;
 }
 
+/// A fake UserCubit for when the user is logged in.
+class FakeUserCubitLoggedIn extends UserCubit {
+  @override
+  get user => User(id: "123", name: "Test User", email: "test@example.com");
+}
+
+/// A fake FavoriteCubit. It simply uses the default state (FavoriteInitial),
+/// which causes the FavoriteScreen to show the "No favorites yet!" UI.
+class FakeFavoriteCubit extends FavoriteCubit {}
+
 void main() {
-  testWidgets('FavoriteScreen UI renders correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MaterialApp(
-        home: MultiBlocProvider(
-          providers: [
-            BlocProvider<UserCubit>(
-              create: (context) => FakeUserCubit(),
-            ),
-            BlocProvider<FavoriteCubit>(
-              create: (context) => FavoriteCubit(),
-            ),
-          ],
-          child: const FavoriteScreen(),
+  group('FavoriteScreen Widget Tests', () {
+    testWidgets('displays login prompt when user is not logged in', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: MultiBlocProvider(
+            providers: [
+              BlocProvider<UserCubit>(
+                create: (_) => FakeUserCubitLoggedOut(),
+              ),
+              BlocProvider<FavoriteCubit>(
+                create: (_) => FakeFavoriteCubit(),
+              ),
+            ],
+            child: const FavoriteScreen(),
+          ),
         ),
-      ),
-    );
+      );
 
-    // Allow all asynchronous events to settle.
-    await tester.pumpAndSettle();
+      // Allow asynchronous events to settle.
+      await tester.pumpAndSettle();
 
-    // Expect the "No favorites yet!" branch to be rendered.
-    expect(find.text("No favorites yet!"), findsOneWidget);
-    expect(find.text("Add some places to your favorites."), findsOneWidget);
+      // When no user is logged in, the login prompt should be shown.
+      expect(find.text("Please login first"), findsOneWidget);
+      expect(find.text("Login"), findsOneWidget);
+    });
+
+    testWidgets('displays no favorites message when logged in and favorites are empty', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: MultiBlocProvider(
+            providers: [
+              BlocProvider<UserCubit>(
+                create: (_) => FakeUserCubitLoggedIn(),
+              ),
+              BlocProvider<FavoriteCubit>(
+                create: (_) => FakeFavoriteCubit(),
+              ),
+            ],
+            child: const FavoriteScreen(),
+          ),
+        ),
+      );
+
+      // Allow asynchronous events to settle.
+      await tester.pumpAndSettle();
+
+      // When logged in but with no favorites, the "No favorites yet!" UI is shown.
+      expect(find.text("No favorites yet!"), findsOneWidget);
+      expect(find.text("Add some places to your favorites."), findsOneWidget);
+    });
   });
 }
