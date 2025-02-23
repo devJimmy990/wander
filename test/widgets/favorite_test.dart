ab1391437d2b8186026f370449e67b8095fe2ffd
Ahmed Roshdy <ahmedroshdy654@gmail.com>
2025-02-23 12:29:20 +0200

    commit update widget tests

--- test/widgets/favorite_test.dart
+++ test/widgets/favorite_test.dart
@@ -2,14 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wander/controller/cubit/user/user_cubit.dart';
+import 'package:wander/controller/cubit/favorite/favorite_cubit.dart';
 import 'package:wander/presentation/features/favorite/page.dart';
+import 'package:wander/data/model/user.dart';
+
+class FakeUserCubit extends UserCubit {
+  @override
+  get user => User(id: "123", name: "Test User", email: "test@gmail.com");
+}
 
 void main() {
   testWidgets('FavoriteScreen UI renders correctly', (WidgetTester tester) async {
     await tester.pumpWidget(
       MaterialApp(
-        home: BlocProvider<UserCubit>(
-          create: (context) => UserCubit(),
+        home: MultiBlocProvider(
+          providers: [
+            BlocProvider<UserCubit>(
+              create: (context) => FakeUserCubit(),
+            ),
+            BlocProvider<FavoriteCubit>(
+              create: (context) => FavoriteCubit(),
+            ),
+          ],
           child: const FavoriteScreen(),
         ),
       ),
@@ -18,6 +32,7 @@ void main() {
     // Allow all asynchronous events to settle.
     await tester.pumpAndSettle();
 
+    // Expect the "No favorites yet!" branch to be rendered.
     expect(find.text("No favorites yet!"), findsOneWidget);
     expect(find.text("Add some places to your favorites."), findsOneWidget);
   });
